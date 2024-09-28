9fd20de4c7c847956fcdaf2d12689a597d46e141
Ivan Vorobyov <ya@ivanvorobev.ru>
2024-09-28 20:01:31 +0300

    feat: add categories component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container, Title } from '@/shared/ui';
+import { Categories } from '@/entities';
 
 const Home: React.FC = () => {
   return (
     <>
       <Container className='mt-10'>
         <Title text='Все пиццы' size='lg' className='font-extrabold' />
+        <Categories />
       </Container>
     </>
   );
---
+++ entities/categories.tsx
@@ -0,0 +1,40 @@
+import { cn } from '@/shared/lib/utils';
+import React from 'react';
+
+interface ICategoriesProps {
+  className?: string;
+}
+
+const categories = [
+  'Пиццы',
+  'Комбо',
+  'Закуски',
+  'Коктейли',
+  'Кофе',
+  'Напитки',
+  'Десерты',
+];
+const activeIndex = 0;
+
+export const Categories: React.FC<ICategoriesProps> = ({ className }) => {
+  return (
+    <ul className={cn('flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
+      {categories.map((categorie, index) => (
+        <li key={index}>
+          <a
+            href='#'
+            className={cn(
+              'flex items-center gap-1 font-bold h-11 px-5 rounded-xl',
+              index === activeIndex &&
+                'bg-white shadow-md shadow-gray-200 text-primary'
+            )}
+          >
+            <button className='flex items-center gap-1 font-bold h-11 px-5 rounded-xl'>
+              {categorie}
+            </button>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
