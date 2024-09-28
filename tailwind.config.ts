a7992f7c5a44e5662860ae94d71933b47813cb90
Ivan Vorobyov <ya@ivanvorobev.ru>
2024-09-28 19:53:22 +0300

    update: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,8 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './shared/**/*.{js,ts,jsx,tsx,mdx}',
+    './entities/**/*.{js,ts,jsx,tsx,mdx}',
+    './features/**/*.{js,ts,jsx,tsx,mdx}',
     './widgets/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
