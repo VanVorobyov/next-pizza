f0d58eab14a92231147f60e24570ed6b9ea44ccd
Ivan Vorobyov <ya@ivanvorobev.ru>
2024-09-29 13:03:25 +0300

    feat: add top bar component and fix indexes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Container, Title } from '@/shared/ui';
-import { Categories } from '@/entities';
-import { SortPopup } from '@/entities/sort-popup';
+import { TopBar } from '@/widgets';
 
 const Home: React.FC = () => {
   return (
     <>
       <Container className='mt-10'>
         <Title text='Все пиццы' size='lg' className='font-extrabold' />
-        <Categories />
-        <SortPopup />
       </Container>
+      <TopBar />
     </>
   );
 };
---
+++ widgets/top-bar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { cn } from '@/shared/lib/utils';
+import { Container } from '@/shared/ui';
+import { Categories, SortPopup } from '@/entities';
+
+interface ITopBarProps {
+  className?: string;
+}
+
+export const TopBar: React.FC<ITopBarProps> = ({ className }) => {
+  return (
+    <div
+      className={cn(
+        'sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10',
+        className
+      )}
+    >
+      <Container>
+        <Categories />
+        <SortPopup />
+      </Container>
+    </div>
+  );
+};
