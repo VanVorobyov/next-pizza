3a0ad163bbbbb57390bf3a5a8614990174cefe5a
Ivan Vorobyov <ya@ivanvorobev.ru>
2025-05-13 22:14:47 +0300

    add pizza seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,8 @@
+import { randomNumber } from '@/shared/lib/utils';
 import { _ingredients, categories, products } from './constants';
 import { prisma } from './prisma-client';
 import { hashSync } from 'bcrypt';
+import { Prisma } from '@prisma/client';
 
 const up = async () => {
   try {
@@ -33,6 +35,124 @@ const up = async () => {
 
     await prisma.product.createMany({
       data: products,
+    });
+
+    const pizza1 = await prisma.product.create({
+      data: {
+        name: 'Пепперони фреш',
+        imageURL:
+          'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+        categoryId: 1,
+        ingredients: {
+          connect: _ingredients.slice(0, 5),
+        },
+      },
+    });
+
+    const pizza2 = await prisma.product.create({
+      data: {
+        name: 'Сырная',
+        imageURL:
+          'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+        categoryId: 1,
+        ingredients: {
+          connect: _ingredients.slice(5, 10),
+        },
+      },
+    });
+
+    const pizza3 = await prisma.product.create({
+      data: {
+        name: 'Чоризо фреш',
+        imageURL:
+          'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+        categoryId: 1,
+        ingredients: {
+          connect: _ingredients.slice(10, 40),
+        },
+      },
+    });
+
+    await prisma.productItem.createMany({
+      data: [
+        // Пицца "Пепперони фреш"
+        {
+          productId: pizza1.id,
+          pizzaType: 1,
+          price: randomNumber(600, 1200),
+          size: 20,
+        },
+        {
+          productId: pizza1.id,
+          pizzaType: 2,
+          price: randomNumber(600, 1200),
+          size: 30,
+        },
+        {
+          productId: pizza1.id,
+          pizzaType: 1,
+          price: randomNumber(600, 1200),
+          size: 40,
+        },
+        // Пицца "Сырная"
+        {
+          productId: pizza2.id,
+          pizzaType: 1,
+          price: randomNumber(800, 1200),
+          size: 20,
+        },
+        {
+          productId: pizza2.id,
+          pizzaType: 1,
+          price: randomNumber(600, 1200),
+          size: 30,
+        },
+        {
+          productId: pizza2.id,
+          pizzaType: 1,
+          price: randomNumber(600, 1200),
+          size: 40,
+        },
+        {
+          productId: pizza2.id,
+          pizzaType: 2,
+          price: randomNumber(800, 1200),
+          size: 20,
+        },
+        {
+          productId: pizza2.id,
+          pizzaType: 2,
+          price: randomNumber(600, 1200),
+          size: 30,
+        },
+        {
+          productId: pizza2.id,
+          pizzaType: 2,
+          price: randomNumber(600, 1200),
+          size: 40,
+        },
+        // Пицца "Чоризо фреш"
+        {
+          productId: pizza3.id,
+          pizzaType: 1,
+          price: randomNumber(600, 1200),
+          size: 20,
+        },
+        {
+          productId: pizza3.id,
+          pizzaType: 2,
+          price: randomNumber(600, 1200),
+          size: 30,
+        },
+        {
+          productId: pizza3.id,
+          pizzaType: 2,
+          price: randomNumber(600, 1200),
+          size: 40,
+        },
+
+        // Остальные продукты
+      ],
     });
   } catch (error) {
     console.error('❌ Error in up function:', error);
