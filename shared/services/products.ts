785090f52fcb54063165c6b7fd4646ee822e94a6
Ivan Vorobyov <ya@ivanvorobev.ru>
2025-06-02 20:57:36 +0300

    chore: add constants to api routes

--- shared/services/products.ts
+++ shared/services/products.ts
@@ -1,10 +1,14 @@
 import { Product } from '@prisma/client';
 import { axiosInstance } from './instance';
+import { ApiRoutes } from './constants';
 
 export const search = async (query: string) => {
-  const { data } = await axiosInstance.get<Product[]>(`products/search`, {
-    params: { query },
-  });
+  const { data } = await axiosInstance.get<Product[]>(
+    ApiRoutes.SEARCH_PRODUCTS,
+    {
+      params: { query },
+    }
+  );
 
   return data;
 };
