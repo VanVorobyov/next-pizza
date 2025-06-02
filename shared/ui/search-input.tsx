ac141f3c31a28df50c472083b7aa3b40b1e9b1a3
Ivan Vorobyov <ya@ivanvorobev.ru>
2025-06-02 21:02:09 +0300

    feat: add products render in search input & improve types

--- shared/ui/search-input.tsx
+++ shared/ui/search-input.tsx
@@ -6,6 +6,7 @@ import { Search } from 'lucide-react';
 import { useClickOutside } from '../hooks';
 import Link from 'next/link';
 import { Api } from '../services/api-client';
+import { Product } from '@prisma/client';
 
 interface ISearchInputProps {
   className?: string;
@@ -16,13 +17,16 @@ export const SearchInput: React.FC<ISearchInputProps> = React.memo(
     const ref = useRef<HTMLDivElement>(null);
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [focused, setFocused] = useState<boolean>(false);
+    const [products, setProducts] = useState<Product[]>([]);
 
     useClickOutside(ref, () => {
       setFocused(false);
     });
 
     useEffect(() => {
-      Api.products.search(searchQuery);
+      Api.products.search(searchQuery).then((data) => {
+        setProducts(data);
+      });
     }, [searchQuery]);
 
     return (
@@ -53,18 +57,20 @@ export const SearchInput: React.FC<ISearchInputProps> = React.memo(
               focused && 'visible opacity-100 top-12'
             )}
           >
-            <Link
-              className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
-              href="/"
-            >
-              <img
-                className="rounded-sm h-8 w-8"
-                src="https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp"
-                alt="пицца"
-              />
+            {products.map((product) => (
+              <Link
+                className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+                href="/"
+              >
+                <img
+                  className="rounded-sm h-8 w-8"
+                  src={product.imageURL}
+                  alt="пицца"
+                />
 
-              <span>name</span>
-            </Link>
+                <span>{product.name}</span>
+              </Link>
+            ))}
           </div>
         </div>
       </>
