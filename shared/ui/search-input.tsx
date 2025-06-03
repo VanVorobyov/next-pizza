c787ef63972ab9b102d18cfe68c173d25f7908fd
Ivan Vorobyov <ya@ivanvorobev.ru>
2025-06-03 20:15:12 +0300

    fix: minor fixes in search input

--- shared/ui/search-input.tsx
+++ shared/ui/search-input.tsx
@@ -19,7 +19,7 @@ export const SearchInput: React.FC<ISearchInputProps> = React.memo(
     const [searchQuery, setSearchQuery] = useState<string>('');
     const [focused, setFocused] = useState<boolean>(false);
     const [products, setProducts] = useState<Product[]>([]);
-    const debouncedQuery = useDebounce(searchQuery, 100);
+    const debouncedQuery = useDebounce(searchQuery, 250);
     const deferredQuery = useDeferredValue(debouncedQuery);
 
     useEffect(() => {
@@ -51,27 +51,30 @@ export const SearchInput: React.FC<ISearchInputProps> = React.memo(
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           ></input>
-          <div
-            className={cn(
-              'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
-              focused && 'visible opacity-100 top-12'
-            )}
-          >
-            {products.map((product) => (
-              <Link
-                className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
-                href={`/product/${product.id}`}
-              >
-                <img
-                  className="rounded-sm h-8 w-8"
-                  src={product.imageURL}
-                  alt={product.name}
-                />
 
-                <span>{product.name}</span>
-              </Link>
-            ))}
-          </div>
+          {products.length > 0 && (
+            <div
+              className={cn(
+                'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
+                focused && 'visible opacity-100 top-12'
+              )}
+            >
+              {products.map((product) => (
+                <Link
+                  className="flex items-center gap-3 w-full px-3 py-2 hover:bg-primary/10"
+                  href={`/product/${product.id}`}
+                >
+                  <img
+                    className="rounded-sm h-8 w-8"
+                    src={product.imageURL}
+                    alt={product.name}
+                  />
+
+                  <span>{product.name}</span>
+                </Link>
+              ))}
+            </div>
+          )}
         </div>
       </>
     );
